9b4609319b47b032d4b4a9b22e51c0885ec17b92
hirokiwa <hirokifsys@gmail.com>
2022-06-10 10:13:42 +0900

    part4

--- part4/shinchan.c
+++ part4/shinchan.c
@@ -2,15 +2,11 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
-
     FILE *fp;
-
     char *argv_p = argv[1];
 
     fp = fopen(argv_p, "w");
-
     fprintf(fp, "クレヨンしんちゃん");
-
     fclose(fp);
     
     return(0);
