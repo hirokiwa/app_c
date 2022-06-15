3c0348fe2b0efbf2d056fe40aa370c406ddd1b8d
hirokiwa <hirokifsys@gmail.com>
2022-06-15 21:26:16 +0900

    debag part6

--- part6/daikei.c
+++ part6/daikei.c
@@ -22,18 +22,29 @@ int main(void){
     data da, db, dc;
 
     puts("台形Aの値を入力せよ");
-    set(&da);
-    puts("台形Bの値を入力せよ");
-    set(&db);
+    printf("下辺＝");
+    scanf("%d", &da.kahen);
+    printf("上辺＝");
+    scanf("%d", &da.jouhen);
+    printf("高さ＝");
+    scanf("%d", &da.takasa);
 
-    printf("台形Aの面積は%.2fです。\n",(double)((double)(da.jouhen + da.kahen) / 2 * da.takasa));
-    printf("台形Bの面積は%.2fです。\n",(double)((double)(db.jouhen + db.kahen) / 2 * db.takasa));
+    puts("台形Bの値を入力せよ");
+    printf("下辺＝");
+    scanf("%d", &db.kahen);
+    printf("上辺＝");
+    scanf("%d", &db.jouhen);
+    printf("高さ＝");
+    scanf("%d", &db.takasa);
+
+    printf("台形Aの面積は%.1fです。\n",(double)((double)(da.jouhen + da.kahen) / 2 * da.takasa));
+    printf("台形Bの面積は%.1fです。\n",(double)((double)(db.jouhen + db.kahen) / 2 * db.takasa));
 
     dc.jouhen = da.jouhen + db.jouhen;
     dc.kahen = da.kahen + db.kahen;
     dc.takasa = da.takasa + db.takasa;
 
-    printf("台形Cの面積は%.2fです。\n",(double)((double)(dc.jouhen + dc.kahen / 2) * dc.takasa));
+    printf("台形Cの面積は%.1fです。\n",(double)((double)(dc.jouhen + dc.kahen / 2) * dc.takasa));
 
     return(0);
 }
